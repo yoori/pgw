28b0eac544763070e20fe1c7d74f1c0b7adda412
yoori <yuri.kuznecov@gmail.com>
2025-06-25 19:50:40 +0000

    compile fix

--- src/radius_server/server.cpp
+++ src/radius_server/server.cpp
@@ -20,6 +20,8 @@ void Server::startReceive()
 RadProto::Packet Server::makeResponse(const RadProto::Packet& request)
 {
     std::vector<RadProto::Attribute*> attributes;
+    std::vector<RadProto::VendorSpecific> vendorSpecific;
+    /*
     attributes.push_back(new RadProto::String(m_dictionaries.attributeCode("User-Name"), "test"));
     attributes.push_back(new RadProto::Integer(m_dictionaries.attributeCode("NAS-Port"), 20));
     std::array<uint8_t, 4> address {127, 104, 22, 17};
@@ -29,9 +31,9 @@ RadProto::Packet Server::makeResponse(const RadProto::Packet& request)
     std::vector<uint8_t> chapPassword {'1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g' };
     attributes.push_back(new RadProto::ChapPassword(m_dictionaries.attributeCode("CHAP-Password"), 1, chapPassword));
 
-    std::vector<RadProto::VendorSpecific> vendorSpecific;
     std::vector<uint8_t> vendorValue {0, 0, 0, 3};
     vendorSpecific.push_back(RadProto::VendorSpecific(m_dictionaries.vendorCode("Dlink"), m_dictionaries.vendorAttributeCode("Dlink", "Dlink-User-Level"), vendorValue));
+    */
 
     if (request.type() == RadProto::ACCESS_REQUEST)
         return RadProto::Packet(RadProto::ACCESS_ACCEPT, request.id(), request.auth(), attributes, vendorSpecific);
