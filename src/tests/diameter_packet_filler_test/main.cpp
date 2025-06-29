d9b08d60d25c6c086ce208f00dda1f5a4fc4747e
yoori <yuri.kuznecov@gmail.com>
2025-06-29 04:44:44 +0000

    added test.

---
+++ src/tests/diameter_packet_filler_test/main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <sstream>
+
+#include <gears/AppUtils.hpp>
+
+#include <dpi/DiameterPacketFiller.hpp>
+
+
+bool test1(dpi::DiameterDictionary& dictionary)
+{
+  dpi::DiameterPacketFiller filler(dictionary, 272);
+  filler.add_avp("User-Equipment-Info.User-Equipment-Info-Value", dpi::Value(std::string("11111")));
+  /*
+  dpi::Value val;
+  val.emplace<int64_t>(11111);
+  filler.add_avp("User-Equipment-Info.User-Equipment-Info-Value", val);
+  */
+
+  Diameter::Packet packet = Diameter::Packet()
+    .setHeader(
+    Diameter::Packet::Header()
+      // Setting that it's request 
+      .setCommandFlags(
+         Diameter::Packet::Header::Flags()
+         .setFlag(Diameter::Packet::Header::Flags::Bits::Request, true)
+      )
+      .setCommandCode(272)
+      .setApplicationId(0)
+      .setHBHIdentifier(0x00000ad1)
+      .setETEIdentifier(0x00000ad1)
+   );
+
+  filler.apply(packet);
+
+  return true;
+}
+
+int main(int argc, char* argv[])
+{
+  Gears::AppUtils::Args args;
+  Gears::AppUtils::StringOption opt_dict;
+  args.add(Gears::AppUtils::equal_name("dict"), opt_dict);
+
+  args.parse(argc - 1, argv + 1);
+
+  dpi::DiameterDictionary dictionary(*opt_dict);
+
+  bool res = true;
+
+  if (!test1(dictionary))
+  {
+    res = false;
+  }
+
+  return res ? 0 : -1;
+}
